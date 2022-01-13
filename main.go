7621dbb73b82aca3ca65b8b078e70295d18f228e
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-01-13 17:26:14 +0300

    Added new Login handler

--- main.go
+++ main.go
@@ -3,19 +3,13 @@ package main
 
 import (
     "net/http"
-    "fmt"
     "mercury/handlers"
-    "mercury/database"
-    "mercury/models"
 )
 
 
 func main() {
-    var user models.User
-    database.DB.First(&user)
-    fmt.Println(user.Username)
-
     http.HandleFunc("/api/register", handlers.Register)
+    http.HandleFunc("/api/login", handlers.Login)
 
     http.ListenAndServe(":8080", nil)
 }
