dd5b53c9d7356f09d82eb8e03fa2ec8b74300c5d
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-01-13 16:58:09 +0300

    Package with utils functions

---
+++ utils/utils.go
@@ -0,0 +1,34 @@
+package utils
+
+
+import (
+    "crypto/rand"
+    "encoding/hex"
+)
+
+// https://gist.github.com/dopey/c69559607800d2f2f90b1b1ed4e550fb
+// GenerateRandomBytes returns securely generated random bytes.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+
+// GenerateRandomStringURLSafe returns a URL-safe, base64 encoded
+// securely generated random string.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomStringURLSafe(n int) (string, error) {
+	b, err := GenerateRandomBytes(n)
+	return hex.EncodeToString(b), err
+}
