0a620753b1fecb80801339df2732ef4df9d81585
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-01-20 14:37:18 +0300

    Created interface for TableName

--- models/models.go
+++ models/models.go
@@ -1,14 +1,19 @@
 package models
 
+import "gorm.io/gorm"
+
+type Tabler interface {
+	TableName() string
+}
 
 type User struct {
-    ID int
-    Username string `json:"username"`
-    Password string `json:"password"`
-    Salt string `json:"salt"`
-    Token string `json:"token"`
+	gorm.Model
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Salt     string `json:"salt"`
+	Token    string `json:"token"`
 }
 
 func (User) TableName() string {
-    return "users"
+	return "users"
 }
