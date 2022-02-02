1c2ca7079fa72dc2226c8197c3159ec3e6ac119b
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-02-02 18:15:17 +0300

    Created models for definitions API

--- models/models.go
+++ models/models.go
@@ -16,3 +16,25 @@ type User struct {
 func (User) TableName() string {
 	return "users"
 }
+
+type Word struct {
+	ID          int
+	Word        string
+	Phonetics   string
+	Definitions []Definition
+}
+
+func (Word) TableName() string {
+	return "words"
+}
+
+type Definition struct {
+	ID         int
+	Definition string
+	Example    string
+	WordID     int
+}
+
+func (Definition) TableName() string {
+	return "definitions"
+}
