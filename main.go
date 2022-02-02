a760c84338536bd07f6fd9e1a37f3c56c6cb3757
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-02-02 19:43:52 +0300

    Added handler for definitions API

--- main.go
+++ main.go
@@ -13,8 +13,9 @@ func main() {
 	router.HandleFunc("/api/register", handlers.Register).Methods("POST")
 	router.HandleFunc("/api/login", handlers.Login).Methods("POST")
 	router.HandleFunc("/api/checkToken/{token}", handlers.CheckToken).Methods("POST")
-	router.HandleFunc("/api/getUserData/{token}", handlers.GetUserData).Methods("POST")
+	router.HandleFunc("/api/getUserData/{token}", handlers.GetUserData).Methods("GET")
 	router.HandleFunc("/api/getUserProfilePicture/{username}", handlers.GetUserProfilePicture).Methods("GET")
+	router.HandleFunc("/api/definition/{word}", handlers.GetDefinition).Methods("GET")
 
 	http.ListenAndServe(":8080", router)
 }
