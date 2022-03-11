a5bfe531b2b32f853db18b5b34d302d5f70f64de
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-11 03:55:11 +0300

    Priority is returned with each word and sorting is done on the client

--- handlers/learning.go
+++ handlers/learning.go
@@ -12,42 +12,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func GetWordsToLearn(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	var user models.User
-	var requestedUser models.User
-	var collection models.Collection
-
-	if status := utils.AuthenticateToken(&w, req, &user, vars["token"]); !status {
-		return
-	}
-
-	database.DB.Where("username = ?", vars["createdByUsername"]).Find(&requestedUser)
-	if requestedUser.Username == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, `{"error": "User was not found"}`)
-		return
-	}
-
-	database.DB.Preload("Words.Word.Definitions").Where("name = ? and user_id = ?", vars["collectionName"], requestedUser.ID).Find(&collection)
-	if collection.Name == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, `{"error": "Collection was not found"}`)
-		return
-	}
-
-	if user.ID != collection.UserID && !user.IsSubscribed {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, `{"error": "Subscribe to see another users collections"}`)
-		return
-	}
-
-	var result []string
-	database.DB.Raw("select w.word from priorities p left join collection_words cw on p.collection_word_id = cw.id left join words w on w.id = cw.word_id order by p.priority limit 20").Scan(&result)
-	response, _ := json.Marshal(result)
-	fmt.Fprint(w, string(response))
-}
-
 func LearnWords(w http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	var user models.User
@@ -92,6 +56,7 @@ func LearnWords(w http.ResponseWriter, req *http.Request) {
 		}
 
 		database.DB.Save(&priority)
+
 	}
 
 }
--- main.go
+++ main.go
@@ -24,7 +24,6 @@ func main() {
 	router.HandleFunc("/api/addWordToCollection/{token}/{collectionName}/{word}", handlers.AddWordToCollection).Methods("POST")
 	router.HandleFunc("/api/deleteCollectionWord/{token}/{collectionName}/{word}", handlers.DeleteCollectionWord).Methods("POST")
 	// router.HandleFunc("/api/deleteWordsFromCollection/{token}/{collectionName}", handlers.DeleteWordsFromCollection).Methods("POST")
-	router.HandleFunc("/api/getWordsToLearn/{token}/{createdByUsername}/{collectionName}", handlers.GetWordsToLearn).Methods("GET")
 	router.HandleFunc("/api/learnWords/{token}/{collectionName}", handlers.LearnWords).Methods("POST")
 
 	origins := gorillaHandlers.AllowedOrigins([]string{"*"})
