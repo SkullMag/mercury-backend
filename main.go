ab26e97d7df89fa71e2c71ebb38629bc6d0ac42a
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-27 13:17:01 +0300

    Now listen on secure https

--- main.go
+++ main.go
@@ -32,5 +32,5 @@ func main() {
 	}
 
 	origins := gorillaHandlers.AllowedOrigins([]string{"*"})
-	http.ListenAndServe(":"+os.Getenv("PORT"), gorillaHandlers.CORS(origins)(router))
+	http.ListenAndServeTLS(":"+os.Getenv("PORT"), "/etc/ssl/mercurydict.com.crt", "/etc/ssl/mercurydict.com.key", gorillaHandlers.CORS(origins)(router))
 }
