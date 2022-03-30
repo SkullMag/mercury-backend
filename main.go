ad0d9ca28e85913988648f9517b2858229574605
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-30 09:15:28 +0300

    Structured code

--- main.go
+++ main.go
@@ -13,26 +13,29 @@ func main() {
 	router := mux.NewRouter()
 
 	api := router.PathPrefix("/api").Subrouter()
-	api.HandleFunc("/signup", handlers.SignUp).Methods("POST")
-	api.HandleFunc("/login", handlers.Login).Methods("POST")
+
+	// DELETE
+	api.HandleFunc("/deleteCollection/{token}/{collectionName}", handlers.DeleteCollection).Methods("DELETE")
+	api.HandleFunc("/deleteCollectionWord/{token}/{collectionName}/{word}", handlers.DeleteCollectionWord).Methods("DELETE")
+
+	// GET
 	api.HandleFunc("/getUserData/{token}", handlers.GetUserData).Methods("GET")
 	api.HandleFunc("/getUserData/{token}/{username}", handlers.GetUserDataByUsername).Methods("GET")
 	api.HandleFunc("/getUserProfilePicture/{username}", handlers.GetUserProfilePicture).Methods("GET")
-	api.HandleFunc("/definition/{word}", handlers.GetDefinition).Methods("GET")
-	api.HandleFunc("/requestVerificationCode/{username}/{email}", handlers.RequestVerificationCode).Methods("GET")
-	api.HandleFunc("/createCollection/{token}/{name}", handlers.CreateCollection).Methods("POST")
-	api.HandleFunc("/deleteCollection/{token}/{collectionName}", handlers.DeleteCollection).Methods("POST")
 	api.HandleFunc("/getCollections", handlers.GetAllCollections).Methods("GET")
 	api.HandleFunc("/getCollections/{token}/{username}", handlers.GetCollections).Methods("GET")
 	api.HandleFunc("/getCollectionWords/{token}/{createdByUsername}/{collectionName}", handlers.GetCollectionWords).Methods("GET")
-	api.HandleFunc("/addWordToCollection/{token}/{collectionName}/{word}", handlers.AddWordToCollection).Methods("POST")
-	api.HandleFunc("/deleteCollectionWord/{token}/{collectionName}/{word}", handlers.DeleteCollectionWord).Methods("POST")
-	api.HandleFunc("/learnWords/{token}/{authorUsername}/{collectionName}", handlers.LearnWords).Methods("POST")
+	api.HandleFunc("/definition/{word}", handlers.GetDefinition).Methods("GET")
+	api.HandleFunc("/requestVerificationCode/{username}/{email}", handlers.RequestVerificationCode).Methods("GET")
 
-	if val, ok := os.LookupEnv("MERCURY_PRODUCTION"); ok && val == "1" {
-		router.PathPrefix("/").Handler(http.FileServer(http.Dir("build")))
-	}
+	// POST
+	api.HandleFunc("/signup", handlers.SignUp).Methods("POST")
+	api.HandleFunc("/login", handlers.Login).Methods("POST")
+	api.HandleFunc("/createCollection/{token}/{name}", handlers.CreateCollection).Methods("POST")
+	api.HandleFunc("/addWordToCollection/{token}/{collectionName}/{word}", handlers.AddWordToCollection).Methods("POST")
+	api.HandleFunc("/learnWords/{token}/{authorUsername}/{collectionName}", handlers.LearnWords).Methods("POST")
 
 	origins := gorillaHandlers.AllowedOrigins([]string{"*"})
 	http.ListenAndServeTLS(":"+os.Getenv("PORT"), "mercurydict.com.crt", "mercurydict.com.key", gorillaHandlers.CORS(origins)(router))
+	// http.ListenAndServe(":"+os.Getenv("PORT"), gorillaHandlers.CORS(origins)(router))
 }
