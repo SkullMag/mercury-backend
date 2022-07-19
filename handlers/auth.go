c31a8dd138110076a74c21ac1b5bee437732aef0
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-19 17:15:04 +0300

    debug

--- handlers/auth.go
+++ handlers/auth.go
@@ -33,11 +33,10 @@ func SignUp(w http.ResponseWriter, req *http.Request) {
         fmt.Fprint(w, string(response))
         return
     }
-    fmt.Println(user.Username, user.Password, user.Email)
 	// Check body of request
 	if user.Username == "" || user.Password == "" || user.Email == "" {
 		w.WriteHeader(http.StatusBadRequest)
-		errorResponse["error"] = "Username or password was not provided"
+		errorResponse["error"] = "Username or password was not provided " + user.Username + " " + user.Password + " " + user.Email
 		response, _ := json.Marshal(errorResponse)
 		fmt.Fprint(w, string(response))
 		return
