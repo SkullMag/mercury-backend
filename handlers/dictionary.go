54ef804550a104a05d714c3bc1d0037c5211b218
SkullMag <rybalko.oleg.123@mail.ru>
2022-09-14 12:05:34 +0300

    feat: added function for retrieving definitions from dictionaryapi.dev

--- handlers/dictionary.go
+++ handlers/dictionary.go
@@ -3,12 +3,101 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"mercury/database"
 	"mercury/models"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
+
+// Models for dictionaryapi.dev
+type Definition struct {
+	Definition string `json:"definition"`
+	Example    string `json:"example"`
+}
+
+type Meaning struct {
+	PartOfSpeech string       `json:"partOfSpeech"`
+	Definitions  []Definition `json:"definitions"`
+}
+
+type Word struct {
+	Word     string    `json:"word"`
+	Meanings []Meaning `json:"meanings"`
+}
+
+type WordNotFoundError struct{}
+
+func (err WordNotFoundError) Error() string {
+	return "Word was not found"
+}
+
+// Function for getting words from dictionaryapi.dev
+func getFreeDictionaryAPIDefinition(word string) error {
+	var err error
+	word = strings.ToLower(word)
+	resp, err := http.Get(fmt.Sprintf("https://api.dictionaryapi.dev/api/v2/entries/en/%s", word))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var words []Word
+	err = json.Unmarshal(body, &words)
+	if err != nil {
+		return err
+	}
+
+	if len(words) == 0 {
+		return &WordNotFoundError{}
+	}
+
+	flag := false
+	for _, w := range words {
+		if w.Word == word {
+			flag = true
+			break
+		}
+	}
+
+	if !flag {
+		return &WordNotFoundError{}
+	}
+
+	var maxWordID int
+	database.DB.Table("words").Select("max(id)").Find(&maxWordID)
+	maxWordID++
+	result := database.DB.Create(&models.Word{ID: maxWordID, Word: word})
+	if result.Error != nil {
+		return result.Error
+	}
+
+	var maxDefinitionID int
+	database.DB.Table("definitions").Select("max(id)").Find(&maxDefinitionID)
+
+	for _, w := range words {
+		for _, meaning := range w.Meanings {
+			for _, definition := range meaning.Definitions {
+				maxDefinitionID++
+				database.DB.Create(&models.Definition{
+					ID:           maxDefinitionID,
+					PartOfSpeech: meaning.PartOfSpeech,
+					Definition:   definition.Definition,
+					Example:      definition.Example,
+					WordID:       maxWordID,
+				})
+			}
+		}
+	}
+	return nil
+}
 
 func GetDefinition(w http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
@@ -16,9 +105,21 @@ func GetDefinition(w http.ResponseWriter, req *http.Request) {
 	var word models.Word
 	database.DB.Where("word = ?", vars["word"]).First(&word)
 	if word.Word == "" {
+		if err := getFreeDictionaryAPIDefinition(vars["word"]); err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, `{"error": "Word was not found"}`)
+			return
+		}
+		var newWord models.Word
+		res := database.DB.Where("word = ?", strings.ToLower(vars["word"])).First(&newWord)
+		if res.RowsAffected > 0 {
+			word = newWord
+		}
+	}
+	if word.Word == "" {
 		// Word wasn't found in database
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, `{"error": "word was not found"}`)
+		fmt.Fprint(w, `{"error": "Word was not found"}`)
 		return
 	}
 	database.DB.Model(&word).Association("Definitions").Find(&word.Definitions)
